388b87d48cf292518065e995ff08b4667e37994f
Matthias Margush <matthias.margush@target.com>
2014-07-15 12:27:25 -0700

    added utility handlers

--- mux/mux.go
+++ mux/mux.go
@@ -1,6 +1,11 @@
 package mux
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/ghost/handlers"
+)
 
 func MethodHandler(verb string, h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
@@ -11,4 +16,34 @@ func MethodHandler(verb string, h http.Handler) http.Handler {
 		}
 	}
 	return http.HandlerFunc(fn)
+}
+
+const StatusUpgradeRequired = 426
+
+func ErrorHandler(w http.ResponseWriter, err error) {
+	log.Printf("Error: %v", err)
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func HttpsRequiredHandler(h http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		if !(r.URL.Scheme == "https" || r.Header.Get("X-Forwarded-Proto") == "https") {
+			log.Printf("Invalid scheme: %v/%v", r.URL, r.Header.Get("X-Forwarded-Proto"))
+			w.WriteHeader(StatusUpgradeRequired)
+		} else {
+			h.ServeHTTP(w, r)
+		}
+	}
+
+	return http.HandlerFunc(fn)
+}
+
+type AuthFunc func(string, string) bool
+
+func BasicAuthHandler(authFunc AuthFunc, realm string, h http.Handler) http.Handler {
+	authFn := func(user, pass string) (interface{}, bool) {
+		return nil, authFunc(user, pass)
+	}
+
+	return handlers.BasicAuthHandler(h, authFn, realm)
 }
